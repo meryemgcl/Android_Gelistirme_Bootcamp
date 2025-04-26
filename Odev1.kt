c5f049e5afdfb2388905af471c6f7c8915d54557
meryem <153759244+meryemgcl@users.noreply.github.com>
2025-04-26 13:40:03 +0300

    Add files via upload

---
+++ Odev1.kt
@@ -0,0 +1,56 @@
+package com.meryemguclu.kotlin_dersleri.degiskenler
+
+fun main() {
+    val sehir = "Erzurum"
+    val ulke =  "Turkiye"
+    val telefon = "0555 555 55 55"
+    val postaKodu = "25000"
+    val email = "meriguclu123@gmail.com"
+    val meslek = "Veri analizi"
+    val stokMiktari = 7
+    val musteriadi = "Murat"
+    val bakiye = 777.22
+    val dogumGunu = "2004-03-30"
+    val maas = 75000
+    val medeniDurum = "Bekar"
+    val urunYorum = "çok güzel"
+    val odemeTarihi = "2025-04-02"
+    val odeme = 28.785
+    val siparisAdeti = 7
+    val arabaModeli = "Posche 718"
+    val kitapadi = "Uçurtma Avcısı"
+    val yayinlamaTarihi = "2022-02-03"
+    val indirimMiktari = 22
+    val odaSayisi = 55
+    val enlem = 33.0044
+    val boylam = 27.0022
+    val urunAdi = "sözlük"
+    val yemekFiyati = 345
+    val marka = "Hermes"
+    val muzikAdi = "sarılırım birine"
+    val videoSuresi = 5
+    val urunPuani = 7.8
+    val resimAdi = "tatli_kediler.png"
+    val dosyaFormati = "PNG"
+    val renk = "kırmızı"
+    val renkkodu = "#ff0000"
+    val telefonModeli = "iphone 8"
+    val ekranBoyutu = 6.1
+    val agirlik = 174
+    val ulusalGun = "10 kasım"
+    val tatilGunu = "pazar"
+    val rezervasyonTarihi = "2025-03-30"
+    val sokakAdi = "Leylak Sokak"
+    val otobusHatti = "45BS"
+    val kalanDakika = 13
+    val takipKodu = "TR777777777"
+    val kuponSuresi = "15 Gün"
+    val kuponKodu = "MERYEM10"
+    val faturaAdresi = "Yenişehir,Erzurum"
+
+
+
+
+
+
+}
